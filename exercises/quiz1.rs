078c0709a7acf410f14bfe151a93c0fb6faf1fa5
bricked <git@bricked.dev>
2023-11-25 23:44:21 +0100

    quiz1: create calculate_price_of_apples function

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    let discounted = amount > 40;
+    if discounted { amount * 1 } else { amount * 2 }
+}
 
 // Don't modify this function!
 #[test]
